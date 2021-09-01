167e0f03975e78a22efb4649e269f9c84c9dc996
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2021-09-01 16:51:14 +0430

    feat(notes): add register view

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic.edit import FormView
 
-# Create your views here.
+from .forms import UserRegisterForm
+
+
+class RegisterView(FormView):
+    template_name = 'notes/register.html'
+    form_class = UserRegisterForm
+
+    def form_valid(self, form):
+        form.save()
+        return super(RegisterView, self).form_valid(form)
