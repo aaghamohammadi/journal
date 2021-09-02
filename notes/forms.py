8d6af10b5353538c281133493cdc8985b655c324
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2021-09-02 07:42:14 +0430

    feat: add customizable user
    instead of using default Django user, we define a customizable user model.

--- notes/forms.py
+++ notes/forms.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 
 class UserRegisterForm(UserCreationForm):
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ['username', 'email', 'password1', 'password2']
