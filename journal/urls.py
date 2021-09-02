390bc0cb5fd2c62fa2c0b64058fd1bc7e1760ad0
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2021-09-02 15:24:47 +0430

    fix: specifying app_name for the notes
    the parameter namespace defined in include function needs an app_name, which should be specified in the app/urls.py

--- journal/urls.py
+++ journal/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('notes/', include('notes.urls', namespace="leads"))
+    path('notes/', include('notes.urls', namespace="notes"))
 ]
--- notes/urls.py
+++ notes/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from .views import RegisterView
 
+app_name = "notes"
+
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
 ]
