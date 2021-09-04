3ea81838b192d96e7d2c9db3139d17e72cd50ff6
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2021-09-04 12:33:26 +0430

    test(notes): add tests for the landing page view

--- notes/tests.py
+++ notes/tests.py
@@ -1,3 +1,15 @@
+from django.shortcuts import reverse
 from django.test import TestCase
 
-# Create your tests here.
+
+class LandingPageViewTest(TestCase):
+
+    def setUp(self):
+        self.response = self.client.get(reverse("landing-page"))
+
+    def test_status_code(self):
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template_name(self):
+        template_name = "notes/landing.html"
+        self.assertTemplateUsed(self.response, template_name)
