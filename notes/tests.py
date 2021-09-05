efdbb65c6eac5dc24ac86aede23b46526e5f0bdd
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2021-09-05 15:09:16 +0430

    test: add test for user registration and login

--- notes/tests.py
+++ notes/tests.py
@@ -1,15 +1,71 @@
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse
 from django.shortcuts import reverse
 from django.test import TestCase
 
+from .forms import UserRegisterForm
+
+User = get_user_model()
+
 
 class LandingPageViewTest(TestCase):
-
-    def setUp(self):
-        self.response = self.client.get(reverse("landing-page"))
-
     def test_status_code(self):
-        self.assertEqual(self.response.status_code, 200)
+        response = self.client.get(reverse("landing-page"))
+        self.assertEqual(response.status_code, 200)
 
     def test_template_name(self):
+        response = self.client.get(reverse("landing-page"))
         template_name = "notes/landing.html"
-        self.assertTemplateUsed(self.response, template_name)
+        self.assertTemplateUsed(response, template_name)
+
+
+class UserRegisterViewTest(TestCase):
+    def test_status_code(self):
+        response = self.client.get(reverse("notes:register"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name(self):
+        response = self.client.get(reverse("notes:register"))
+        template_name = "notes/register.html"
+        self.assertTemplateUsed(response, template_name)
+
+    def test_register_view_uses_custom_user_register_form(self):
+        response = self.client.get(reverse("notes:register"))
+        self.assertIsInstance(response.context["form"], UserRegisterForm)
+
+    def test_successful_registration_should_redirect_user_to_success_page(self):
+        template_name = "notes/success.html"
+        data = {
+            "username": "sarahkhosravi",
+            "email": "sarah@gmail.com",
+            "password1": "Rted#2s@mks",
+            "password2": "Rted#2s@mks",
+        }
+
+        response = self.client.post(reverse("notes:register"), data, follow=True)
+
+        self.assertRedirects(response, reverse("notes:success"))
+        self.assertIsInstance(response, HttpResponse)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name)
+
+        user = User.objects.filter(username=data["username"]).first()
+
+        self.assertEqual(user.email, data["email"])
+
+
+class SuccessViewTest(TestCase):
+    def test_status_code(self):
+        response = self.client.get(reverse("notes:success"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name(self):
+        response = self.client.get(reverse("notes:success"))
+        template_name = "notes/success.html"
+        self.assertTemplateUsed(response, template_name)
+
+
+class LoginViewTest(TestCase):
+    def test_status_code(self):
+        response = self.client.get(reverse("notes:login"))
+        self.assertEqual(response.status_code, 200)
