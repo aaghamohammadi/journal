7213b3d4a13b303f92ef957ba7c59dde71755940
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2021-09-16 13:27:20 +0430

    feat(notes): define Item model

--- notes/admin.py
+++ notes/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 
+from .models import Item
+
 admin.site.register(get_user_model())
+admin.site.register(Item)
--- notes/models.py
+++ notes/models.py
@@ -4,3 +4,12 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
